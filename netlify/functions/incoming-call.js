09ef055ccaa5fa3473ed625ecb79b201d006253a
CJB <chris.berno@gmail.com>
2023-09-30 19:55:36 -0400

    more /functions/incoming-call.js updates

--- netlify/functions/incoming-call.js
+++ netlify/functions/incoming-call.js
@@ -1,20 +1,6 @@
-const fetch = require('node-fetch');
-const twilio = require('twilio');
-const qs = require('querystring');
-
-const VoiceResponse = twilio.twiml.VoiceResponse;
-
 exports.handler = async () => {
-
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'text/xml',
-    },
-    body: `
-    <Response>
-    <Say>Welcome to the Donut on Demand I V R!</Say>
-    </Response>
-    `,
+    body: 'ok',
   };
 };
