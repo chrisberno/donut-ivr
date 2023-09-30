567b5145eb8daa0812be6fc6f551b31b125eac42
CJB <chris.berno@gmail.com>
2023-09-30 19:12:13 -0400

    /functions/incoming-call.js edits

--- netlify/functions/incoming-call.js
+++ netlify/functions/incoming-call.js
@@ -4,28 +4,17 @@ const qs = require('querystring');
 
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
-exports.handler = async (event) => {
-  const types = await fetch(`${process.env.URL}/types`).then((res) =>
-    res.json(),
-  );
-
-  const twiml = new VoiceResponse();
-
-  const gather = twiml.gather({
-    action: '/.netlify/functions/step-two',
-  });
-
-  gather.say('Please choose from one of the following options.');
-
-  types.forEach((donutType, index) => {
-    gather.say(`Press ${index + 1} for ${donutType}`);
-  });
+exports.handler = async () => {
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/xml',
     },
-    body: twiml.toString(),
+    body: `
+    <Response>
+    <Say>Welcome to the Donut on Demand I V R!</Say>
+    </Response>
+    `,
   };
 };
